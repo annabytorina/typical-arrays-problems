1b4c6e36355a7b0d94b0f040d55270076ff0bd63
Ania <hannabutorina@gmail.com>
2020-10-03 22:40:26 +0300

    prefactor: add new files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
 exports.min = function min (array) {
-  return 0;
+  let min;
+  if (array == undefined || array == 0) return 0;
+  for (let i = 0;i < array.length; i++){
+    if (i == 0) min = array[i];
+    if (array[i] < min ){
+      min = array[i];
+    }
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let max;
+  if (array == undefined || array == 0) return 0;
+  for (let i = 0;i < array.length; i++){
+    if (i == 0) max = array[i];
+    if (array[i] > max ){
+      max = array[i];
+    }
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let avarage;
+  let sumArray = 0;
+  if (array == undefined || array == 0) return 0;
+  for (let i = 0; i < array.length; i++){
+    sumArray = sumArray + array[i];
+  }
+  avarage = sumArray/array.length;
+  return avarage;
 }
